589894e7d23343e115387c2304af7665d0749c2c
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-09-30 16:50:27 +0400

    create component for adding players

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,25 @@
 <script>
   import Navbar from './Navbar.svelte';
   import Player from './Player.svelte';
+  import AddPlayer from './AddPlayer.svelte';
 
   let players = [
     {name: 'Jon Snow', points: 113},
     {name: 'Karl Drogo', points: 46},
     {name: 'Khalesee', points: 174}
   ];
+
+  const addPlayer = (e) => {
+    const newPlayer = e.detail;
+    players = [...players, newPlayer]
+  }
 </script>
 
 
 
 <Navbar />
 <div class="container">
+  <AddPlayer on:addplayer={addPlayer} />
   {#if players.length === 0}
     <p>No Players</p>
   {:else}
